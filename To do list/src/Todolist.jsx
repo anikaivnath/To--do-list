4327c916f2b40663e3cb6197359b076ef352ea4c
anikaivnath <anikaivnath8@gmail.com>
2024-09-13 13:00:09 +0600

    first commit

---
+++ To do list/src/Todolist.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Todolist = () => {
+
+
+
+
+
+
+
+
+
+  return (
+    <>
+    
+    <div className='bg-indigo-950 h-72 w-5/12 justify-center mt-36 px ml-96 px rounded-3xl'>
+    <h1 className='text-3xl text-center pt-5 px'>Get things Done</h1>
+    <div className='flex justify-center mt-2'>
+    <input type='text' placeholder='Input your task' className='border 2 p-2 '></input>
+    <p className='bg-blue-800  text-white   p-2'>Add task</p>
+    </div>
+    </div>
+
+    </>
+  )
+}
+
+export default Todolist
